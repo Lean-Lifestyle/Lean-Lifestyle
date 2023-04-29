ef076a15b53d7eaee74c9c832fc2853b2c099d8c
luisc8487 <lf9904@gmail.com>
2023-04-29 16:56:00 -0400

    migration

---
+++ src/db/migrations/20230429204428_create_user_table.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id').primary();
+        table.string('username').notNullable().unique();
+        table.string('password').notNullable();
+        table.string('email').notNullable().unique();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('gender').notNullable();
+        table.date('date_of_birth').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+    return knex.schema.dropTable('users');
+};
