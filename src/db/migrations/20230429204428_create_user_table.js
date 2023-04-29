b90bfedf179f4fd564dde2e7cf4e3abcc03790f5
Nayanp35 <prajapatinayan35@gmail.com>
2023-04-29 17:20:51 -0400

    created migration and seed and removed eslint

--- src/db/migrations/20230429204428_create_user_table.js
+++ src/db/migrations/20230429204428_create_user_table.js
@@ -3,17 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = (knex) => {
-    return knex.schema.createTable('users', (table) => {
-        table.increments('id').primary();
-        table.string('username').notNullable().unique();
-        table.string('password').notNullable();
-        table.string('email').notNullable().unique();
-        table.string('first_name').notNullable();
-        table.string('last_name').notNullable();
-        table.string('gender').notNullable();
-        table.date('date_of_birth').notNullable();
-        table.timestamp('created_at').defaultTo(knex.fn.now());
-    });
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("username").notNullable().unique();
+    table.string("password").notNullable();
+    table.string("email").notNullable().unique();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("gender").notNullable();
+    table.date("date_of_birth").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
 /**
@@ -21,5 +21,5 @@ exports.up = (knex) => {
  * @returns { Promise<void> }
  */
 exports.down = (knex) => {
-    return knex.schema.dropTable('users');
+  return knex.schema.dropTable("users");
 };
