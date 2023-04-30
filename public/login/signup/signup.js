1037578bcc927f38d80909bc772fcd3a7119c579
Nayanp35 <prajapatinayan35@gmail.com>
2023-04-30 02:08:12 -0400

    signup works

--- public/login/signup/signup.js
+++ public/login/signup/signup.js
@@ -1,4 +1,3 @@
-
 const form = document.querySelector("#form");
 const firstNameInput = document.querySelector("#first-name");
 const lastNameInput = document.querySelector("#last-name");
@@ -9,20 +8,18 @@ const genderInput = document.querySelector("#gender");
 const birthdayInput = document.querySelector("#birthday");
 const login = document.querySelector("#login");
 
-const handleFetch = async (url, options) => {
-    try {
-      const response = await fetch(url, options);
-      const { status, statusText, ok } = response;
-      if (!ok) return [null, { status, statusText }];
-  
-      const content = (status === 204) || await response.json();
-      return [content, null];
-    } catch (error) {
-      return [null, error];
-    }
+const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error(response.statusText);
+    if (response.status === 204) return [{}];
+    return [await response.json()];
+  } catch (error) {
+    return [null, error];
+  }
 };
 
-const handleError = (error) => alert(error.message);
+const handleError = (error) => console.error(error.message);
 
 const data = [
   firstNameInput,
@@ -43,14 +40,16 @@ const getValues = () => {
     username: usernameInput.value,
     password: passwordInput.value,
     gender: genderInput.value,
-    birthday: birthdayInput.value,
+    date_of_birth: birthdayInput.value,
   };
 };
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
   console.log("checking start");
+
   const values = getValues();
+  console.log(values);
   const option = {
     method: "POST",
     headers: {
@@ -58,11 +57,8 @@ form.addEventListener("submit", async (e) => {
     },
     body: JSON.stringify(values),
   };
-  const [data, error] = await handleFetch("/api/users", option);
-
+  const [data, error] = await fetchData("/api/users", option);
   if (error) return handleError(error);
   console.log(data);
   console.log("checking end");
-  window.location.href = "/";
-  eraseValues();
 });
--- src/controllers/user/create.js
+++ src/controllers/user/create.js
@@ -2,13 +2,29 @@ const createUser = async (req, res) => {
   const {
     session,
     db: { User },
-    body: { username, password, email, first_name, last_name, gender, date_of_birth },
+    body: {
+      username,
+      password,
+      email,
+      first_name,
+      last_name,
+      gender,
+      date_of_birth,
+    },
   } = req;
-
-  const user = await User.create(username, password, email, first_name, last_name, gender, date_of_birth);
+  const user = await User.create({
+    username,
+    password,
+    email,
+    first_name,
+    last_name,
+    gender,
+    date_of_birth,
+  });
+  user
+    ? res.status(201).send(user)
+    : res.status(500).send({ err: "Can't create" });
   session.userId = user.id;
-
-  res.send(user);
 };
 
 module.exports = createUser;
