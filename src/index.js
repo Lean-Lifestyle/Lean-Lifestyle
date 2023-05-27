b389286317379fbdc9996aca1cac2b075488a07e
Nayan Prajapati <111540429+Nayanp35@users.noreply.github.com>
2023-05-26 22:56:07 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const server = require("./server");
 
 const port = process.env.PORT || 3000;
-const host = process.env.HOST || "lean-lifestyle" || "localhost" || "127.0.0.1";
+const host = process.env.HOST || "localhost" || "127.0.0.1";
 
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
