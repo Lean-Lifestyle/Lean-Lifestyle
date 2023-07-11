7f8b8a02fb2461262fff07c2f528fa4a7a33daee
Nayanp35 <prajapatinayan35@gmail.com>
2023-07-11 12:49:01 -0400

    rechanged the knex and index file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ const server = require("./server");
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
